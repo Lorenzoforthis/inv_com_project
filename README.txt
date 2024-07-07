3b40c5f40132000db7ffef396c90625997375100
unknown <lorenzoforthis@gmail.com>
2024-07-07 16:20:55 +0800

    共同專案設立first trial

---
+++ README.txt
@@ -0,0 +1,3 @@
+1. css 導入bootstrap 5.0版本
+2. css 載入chart.js模組
+3. chart JS在canvas tag執行
---
+++ canvas.js
@@ -0,0 +1,11 @@
+//canvas 模板
+
+//設置ID與文字樣式
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#009578";
+ctx.font = "bold 24px sans-serif";
+ctx.fillText("hello. world",100,150);
+
+
