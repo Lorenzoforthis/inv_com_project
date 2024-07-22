5f4571aa71774cb6d64bb10dd4512aa8f0bf7442
Lorenzo <lorenzoforthis@gmail.com>
2024-07-22 16:05:06 +0800

    API資料串接完成，下一步導入圖表

--- js/canvas.js
+++ js/canvas.js
@@ -6,6 +6,6 @@ const ctxt = canvas.getContext("2d");
 
 ctxt.fillStyle = "#009578";
 ctxt.font = "bold 24px sans-serif";
-ctxt.fillText("hello. world",100,150);
+ctxt.fillText("這框架文字來自於canvas.js檔,需討論下文字檔是否在html改,還是另創一個專門改文字的檔案",150,150);
 
 
